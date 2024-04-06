7912e87dbad6b8a415306595d90cc4a33252d03e
pagoru <pagoru@gmail.com>
2024-04-06 23:36:55 +0200

    fixes and removed old

--- old/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var React = require("react");
-var ReactDOM = require("react-dom");
-var application_1 = require("./application");
-ReactDOM.render(React.createElement(application_1.default, null), document.getElementById('robot'));
-//# sourceMappingURL=index.js.map
